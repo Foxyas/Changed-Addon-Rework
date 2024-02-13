885a62baf34cc078d6533458a81b7d01489c1d61
Foxyas <CarlosDeck9474@gmail.com>
2024-02-12 23:23:00 -0300

    update files 2.5.4
    JEI SUPORT

--- src/main/java/net/foxyas/changedaddon/extension/JeiSuport.java
+++ src/main/java/net/foxyas/changedaddon/extension/JeiSuport.java
@@ -1,9 +1,8 @@
-/*
-  
 package net.foxyas.changedaddon.extension;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaTypes;
@@ -23,6 +22,7 @@ import net.minecraft.core.NonNullList;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.SimpleContainer;
@@ -44,11 +44,10 @@ import java.util.Objects;
 import net.minecraftforge.fml.common.Mod;
 
 
-public class JeiSuport {  
 
-
-    @JeiPlugin
-    class ChangedAddonModJeiPlugin implements IModPlugin {
+ 
+   @JeiPlugin
+	public class JeiSuport implements IModPlugin {
 		public static mezz.jei.api.recipe.RecipeType<JeiCatalyzerRecipe> JeiCatalyzer_Type = new mezz.jei.api.recipe.RecipeType<>(JeiCatalyzerRecipeCategory.UID, JeiCatalyzerRecipe.class);
         public static mezz.jei.api.recipe.RecipeType<JeiUnifuserRecipe> JeiUnifuser_Type = new mezz.jei.api.recipe.RecipeType<>(JeiUnifuserRecipeCategory.UID, JeiUnifuserRecipe.class);
 
@@ -68,9 +67,9 @@ public class JeiSuport {
         @Override
         public void registerRecipes(IRecipeRegistration registration) {
             RecipeManager recipeManager = Objects.requireNonNull(Minecraft.getInstance().level).getRecipeManager();
-            List<JeiCatalyzerRecipe> JeiCatalyzerRecipes = recipeManager.getAllRecipesFor(JeiSuport.JeiCatalyzerRecipe.Type.INSTANCE);
+            List<JeiCatalyzerRecipe> JeiCatalyzerRecipes = recipeManager.getAllRecipesFor(JeiCatalyzerRecipe.Type.INSTANCE);
             registration.addRecipes(JeiCatalyzer_Type, JeiCatalyzerRecipes);
-            List<JeiUnifuserRecipe> JeiUnifuserRecipes = recipeManager.getAllRecipesFor(JeiSuport.JeiUnifuserRecipe.Type.INSTANCE);
+            List<JeiUnifuserRecipe> JeiUnifuserRecipes = recipeManager.getAllRecipesFor(JeiUnifuserRecipe.Type.INSTANCE);
             registration.addRecipes(JeiUnifuser_Type, JeiUnifuserRecipes);
         }
 
@@ -80,6 +79,10 @@ public class JeiSuport {
             registration.addRecipeCatalyst(new ItemStack(ChangedAddonModBlocks.UNIFUSER.get().asItem()), JeiUnifuser_Type);
         }
     }
+
+
+
+    
 
     @Mod.EventBusSubscriber(modid = ChangedAddonMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
     class ChangedAddonModRecipeTypes {
@@ -99,23 +102,23 @@ public class JeiSuport {
     @Deprecated
     class JeiCatalyzerRecipeCategory implements IRecipeCategory<JeiCatalyzerRecipe> {
         public final static ResourceLocation UID = new ResourceLocation("changed_addon", "jei_catalyzer");
-        public final static ResourceLocation TEXTURE = new ResourceLocation("changed_addon", "textures/screens/catalyzer_screen.png");
+        public final static ResourceLocation TEXTURE = new ResourceLocation("changed_addon", "textures/screens/jei_catalyzer_screen.png");
         private final IDrawable background;
         private final IDrawable icon;
 
         public JeiCatalyzerRecipeCategory(IGuiHelper helper) {
-            this.background = helper.createDrawable(TEXTURE, 0, 0, 200, 86);
+            this.background = helper.createDrawable(TEXTURE, 0, 0, 116, 54);
             this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ChangedAddonModBlocks.CATLYZER.get().asItem()));
         }
 
         @Override
         public mezz.jei.api.recipe.RecipeType<JeiCatalyzerRecipe> getRecipeType() {
-            return net.foxyas.changedaddon.extension.JeiSuport.ChangedAddonModJeiPlugin.JeiCatalyzer_Type;
+            return JeiSuport.JeiCatalyzer_Type;
         }
 
         @Override
         public Component getTitle() {
-            return new TextComponent("Jei Catalyzer");
+            return new TextComponent((new TranslatableComponent("block.changed_addon.catlyzer").getString()));
         }
 
         @Override
@@ -141,12 +144,12 @@ public class JeiSuport {
 
         @Override
         public void setRecipe(IRecipeLayoutBuilder builder, JeiCatalyzerRecipe recipe, IFocusGroup focuses) {
-            builder.addSlot(RecipeIngredientRole.INPUT, 23, 44).addIngredients(recipe.getIngredients().get(0));
-            builder.addSlot(RecipeIngredientRole.OUTPUT, 153, 44).addItemStack(recipe.getResultItem());
+            builder.addSlot(RecipeIngredientRole.INPUT, 12, 18).addIngredients(recipe.getIngredients().get(0));
+            builder.addSlot(RecipeIngredientRole.OUTPUT, 96, 18).addItemStack(recipe.getResultItem());
         }
     }
 
-    static class JeiCatalyzerRecipe implements Recipe<SimpleContainer> {
+    class JeiCatalyzerRecipe implements Recipe<SimpleContainer> {
         private final ResourceLocation id;
         private final ItemStack output;
         private final NonNullList<Ingredient> recipeItems;
@@ -160,22 +163,25 @@ public class JeiSuport {
         @Override
 		public boolean matches(SimpleContainer pContainer, Level pLevel) {
     	if (pLevel.isClientSide()) {
-    	    return false;
+        return false;
+    		}
+
+   	    // Verifica se a lista de ingredientes não está vazia
+   		 if (!recipeItems.isEmpty()) {
+     	   // Verifica se o contêiner possui pelo menos dois slots
+         if (pContainer.getContainerSize() > 1) {
+            // Percorre todos os itens da lista de ingredientes
+            for (Ingredient ingredient : recipeItems) {
+                // Verifica se pelo menos um item da lista atende às condições
+                if (ingredient.test(pContainer.getItem(1))) {
+       	             return true;
+          	      }
+       	     }
+     	   }
    		 }
 
-    	// Verifica se a lista de ingredientes não está vazia
-    	if (!recipeItems.isEmpty()) {
-        // Percorre todos os itens da lista de ingredientes
-        for (Ingredient ingredient : recipeItems) {
-            // Verifica se pelo menos um item da lista atende às condições
-            if (ingredient.test(pContainer.getItem(1))) {
-                return true;
-            			}
-        			}
-    			}
-
-    			return false; // Retorna false se a lista de ingredientes estiver vazia ou nenhum item atender às condições
-			}
+   		 return false; // Retorna false se a lista de ingredientes estiver vazia ou nenhum item atender às condições
+		}
 
         @Override
         public NonNullList<Ingredient> getIngredients() {
@@ -204,49 +210,48 @@ public class JeiSuport {
 
         @Override
         public RecipeType<?> getType() {
-            return net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe.Type.INSTANCE;
+            return net.foxyas.changedaddon.extension.JeiCatalyzerRecipe.Type.INSTANCE;
         }
 
         @Override
         public RecipeSerializer<?> getSerializer() {
-            return net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe.Serializer.INSTANCE;
+            return net.foxyas.changedaddon.extension.JeiCatalyzerRecipe.Serializer.INSTANCE;
         }
 
-        public static class Type implements RecipeType<net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe> {
+        public static class Type implements RecipeType<net.foxyas.changedaddon.extension.JeiCatalyzerRecipe> {
             private Type() {
             }
 
-            public static final net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe.Type INSTANCE = new net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe.Type();
+            public static final net.foxyas.changedaddon.extension.JeiCatalyzerRecipe.Type INSTANCE = new net.foxyas.changedaddon.extension.JeiCatalyzerRecipe.Type();
             public static final String ID = "jei_catalyzer";
         }
 
-        public static class Serializer implements RecipeSerializer<net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe>, IForgeRegistryEntry<RecipeSerializer<?>> {
-            public static final net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe.Serializer INSTANCE = new net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe.Serializer();
+        public static class Serializer implements RecipeSerializer<net.foxyas.changedaddon.extension.JeiCatalyzerRecipe>, IForgeRegistryEntry<RecipeSerializer<?>> {
+            public static final net.foxyas.changedaddon.extension.JeiCatalyzerRecipe.Serializer INSTANCE = new net.foxyas.changedaddon.extension.JeiCatalyzerRecipe.Serializer();
             public static final ResourceLocation ID = new ResourceLocation("changed_addon", "jei_catalyzer");
 
             @Override
-            public net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe fromJson(ResourceLocation pRecipeId, JsonObject pSerializedRecipe) {
+            public net.foxyas.changedaddon.extension.JeiCatalyzerRecipe fromJson(ResourceLocation pRecipeId, JsonObject pSerializedRecipe) {
                 ItemStack output = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(pSerializedRecipe, "output"));
                 JsonArray ingredients = GsonHelper.getAsJsonArray(pSerializedRecipe, "ingredients");
-                NonNullList<Ingredient> inputs = NonNullList.withSize(2, Ingredient.EMPTY);
-                for (int i = 0; i < inputs.size(); i++) {
-                    inputs.set(i, Ingredient.fromJson(ingredients.get(i)));
-                }
-                return new net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe(pRecipeId, output, inputs);
+                NonNullList<Ingredient> inputs = NonNullList.withSize(1, Ingredient.EMPTY);
+                inputs.set(0, Ingredient.fromJson(ingredients.get(0)));
+
+                return new net.foxyas.changedaddon.extension.JeiCatalyzerRecipe(pRecipeId, output, inputs);
             }
 
             @Override
-            public @Nullable net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe fromNetwork(ResourceLocation id, FriendlyByteBuf buf) {
+            public @Nullable net.foxyas.changedaddon.extension.JeiCatalyzerRecipe fromNetwork(ResourceLocation id, FriendlyByteBuf buf) {
                 NonNullList<Ingredient> inputs = NonNullList.withSize(buf.readInt(), Ingredient.EMPTY);
                 for (int i = 0; i < inputs.size(); i++) {
                     inputs.set(i, Ingredient.fromNetwork(buf));
                 }
                 ItemStack output = buf.readItem();
-                return new net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe(id, output, inputs);
+                return new net.foxyas.changedaddon.extension.JeiCatalyzerRecipe(id, output, inputs);
             }
 
             @Override
-            public void toNetwork(FriendlyByteBuf buf, net.foxyas.changedaddon.extension.JeiSuport.JeiCatalyzerRecipe recipe) {
+            public void toNetwork(FriendlyByteBuf buf, net.foxyas.changedaddon.extension.JeiCatalyzerRecipe recipe) {
                 buf.writeInt(recipe.getIngredients().size());
                 for (Ingredient ing : recipe.getIngredients()) {
                     ing.toNetwork(buf);
@@ -274,23 +279,23 @@ public class JeiSuport {
     @Deprecated
     class JeiUnifuserRecipeCategory implements IRecipeCategory<JeiUnifuserRecipe> {
         public final static ResourceLocation UID = new ResourceLocation("changed_addon", "jei_unifuser");
-        public final static ResourceLocation TEXTURE = new ResourceLocation("changed_addon", "textures/screens/unifuserscreen.png");
+        public final static ResourceLocation TEXTURE = new ResourceLocation("changed_addon", "textures/screens/jei_unifuser_screen.png");
         private final IDrawable background;
         private final IDrawable icon;
 
         public JeiUnifuserRecipeCategory(IGuiHelper helper) {
-            this.background = helper.createDrawable(TEXTURE, 0, 0, 200, 104);
+            this.background = helper.createDrawable(TEXTURE, 0, 0, 116, 54);
             this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ChangedAddonModBlocks.UNIFUSER.get().asItem()));
         }
 
         @Override
         public mezz.jei.api.recipe.RecipeType<JeiUnifuserRecipe> getRecipeType() {
-            return net.foxyas.changedaddon.extension.JeiSuport.ChangedAddonModJeiPlugin.JeiUnifuser_Type;
+            return JeiSuport.JeiUnifuser_Type;
         }
 
         @Override
         public Component getTitle() {
-            return new TextComponent("Test");
+            return new TextComponent((new TranslatableComponent("block.changed_addon.unifuser").getString()));
         }
 
         @Override
@@ -323,7 +328,7 @@ public class JeiSuport {
         }
     }
 
-    static class JeiUnifuserRecipe implements Recipe<SimpleContainer> {
+    class JeiUnifuserRecipe implements Recipe<SimpleContainer> {
         private final ResourceLocation id;
         private final ItemStack output;
         private final NonNullList<Ingredient> recipeItems;
@@ -345,7 +350,7 @@ public class JeiSuport {
         // Percorre todos os itens da lista de ingredientes
         for (Ingredient ingredient : recipeItems) {
             // Verifica se pelo menos um item da lista atende às condições
-            if (ingredient.test(pContainer.getItem(1))) {
+            if (ingredient.test(pContainer.getItem(3))) {
                 return true;
             			}
         			}
@@ -381,49 +386,55 @@ public class JeiSuport {
 
         @Override
         public RecipeType<?> getType() {
-            return net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe.Type.INSTANCE;
+            return net.foxyas.changedaddon.extension.JeiUnifuserRecipe.Type.INSTANCE;
         }
 
         @Override
         public RecipeSerializer<?> getSerializer() {
-            return net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe.Serializer.INSTANCE;
+            return net.foxyas.changedaddon.extension.JeiUnifuserRecipe.Serializer.INSTANCE;
         }
 
-        public static class Type implements RecipeType<net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe> {
+        public static class Type implements RecipeType<net.foxyas.changedaddon.extension.JeiUnifuserRecipe> {
             private Type() {
             }
 
-            public static final net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe.Type INSTANCE = new net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe.Type();
+            public static final net.foxyas.changedaddon.extension.JeiUnifuserRecipe.Type INSTANCE = new net.foxyas.changedaddon.extension.JeiUnifuserRecipe.Type();
             public static final String ID = "jei_unifuser";
         }
 
-        public static class Serializer implements RecipeSerializer<net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe>, IForgeRegistryEntry<RecipeSerializer<?>> {
-            public static final net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe.Serializer INSTANCE = new net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe.Serializer();
-            public static final ResourceLocation ID = new ResourceLocation("changed_addon", "jei_unifuser");
+        public static class Serializer implements RecipeSerializer<net.foxyas.changedaddon.extension.JeiUnifuserRecipe>, IForgeRegistryEntry<RecipeSerializer<?>> {
+   		public static final net.foxyas.changedaddon.extension.JeiUnifuserRecipe.Serializer INSTANCE = new net.foxyas.changedaddon.extension.JeiUnifuserRecipe.Serializer();
+   		public static final ResourceLocation ID = new ResourceLocation("changed_addon", "jei_unifuser");
+
+   		@Override
+    	public net.foxyas.changedaddon.extension.JeiUnifuserRecipe fromJson(ResourceLocation pRecipeId, JsonObject pSerializedRecipe) {
+      	  ItemStack output = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(pSerializedRecipe, "output"));
+     	   JsonArray ingredients = GsonHelper.getAsJsonArray(pSerializedRecipe, "ingredients");
+      	  NonNullList<Ingredient> inputs = NonNullList.withSize(ingredients.size(), Ingredient.EMPTY);
+
+     	   for (int i = 0; i < ingredients.size(); i++) {
+      	      JsonElement ingredientElement = ingredients.get(i);
+     	       Ingredient ingredient = Ingredient.fromJson(ingredientElement);
+        	    inputs.set(i, ingredient);
+       			 }
+
+      		  return new net.foxyas.changedaddon.extension.JeiUnifuserRecipe(pRecipeId, output, inputs);
+   			 }
+		
+
 
             @Override
-            public net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe fromJson(ResourceLocation pRecipeId, JsonObject pSerializedRecipe) {
-                ItemStack output = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(pSerializedRecipe, "output"));
-                JsonArray ingredients = GsonHelper.getAsJsonArray(pSerializedRecipe, "ingredients");
-                NonNullList<Ingredient> inputs = NonNullList.withSize(4, Ingredient.EMPTY);
-                for (int i = 0; i < inputs.size(); i++) {
-                    inputs.set(i, Ingredient.fromJson(ingredients.get(i)));
-                }
-                return new net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe(pRecipeId, output, inputs);
-            }
-
-            @Override
-            public @Nullable net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe fromNetwork(ResourceLocation id, FriendlyByteBuf buf) {
+            public @Nullable net.foxyas.changedaddon.extension.JeiUnifuserRecipe fromNetwork(ResourceLocation id, FriendlyByteBuf buf) {
                 NonNullList<Ingredient> inputs = NonNullList.withSize(buf.readInt(), Ingredient.EMPTY);
                 for (int i = 0; i < inputs.size(); i++) {
                     inputs.set(i, Ingredient.fromNetwork(buf));
                 }
                 ItemStack output = buf.readItem();
-                return new net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe(id, output, inputs);
+                return new net.foxyas.changedaddon.extension.JeiUnifuserRecipe(id, output, inputs);
             }
 
             @Override
-            public void toNetwork(FriendlyByteBuf buf, net.foxyas.changedaddon.extension.JeiSuport.JeiUnifuserRecipe recipe) {
+            public void toNetwork(FriendlyByteBuf buf, net.foxyas.changedaddon.extension.JeiUnifuserRecipe recipe) {
                 buf.writeInt(recipe.getIngredients().size());
                 for (Ingredient ing : recipe.getIngredients()) {
                     ing.toNetwork(buf);
@@ -447,5 +458,3 @@ public class JeiSuport {
             }
         }
     }
-}
-*/
