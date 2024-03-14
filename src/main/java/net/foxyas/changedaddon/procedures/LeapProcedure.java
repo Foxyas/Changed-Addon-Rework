be7b0cff2c8b44012b8cc02c68463bd7f9fb1c6f
Foxyas <CarlosDeck9474@gmail.com>
2024-03-14 17:12:25 -0300

    update more files of leap feature

--- src/main/java/net/foxyas/changedaddon/procedures/LeapProcedure.java
+++ src/main/java/net/foxyas/changedaddon/procedures/LeapProcedure.java
@@ -1,8 +1,11 @@
 package net.foxyas.changedaddon.procedures;
 
+import net.minecraft.world.level.GameType;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.client.Minecraft;
 
 import net.foxyas.changedaddon.network.ChangedAddonModVariables;
 
@@ -22,32 +25,58 @@ public class LeapProcedure {
 		double Yspeed = 0;
 		if ((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).transfur) {
 			if (CanLeapProcedure.execute(entity)) {
-				if (entity.isOnGround() && !entity.isInWater()) {
-					if (!entity.isShiftKeyDown()) {
-						deltaX = -Math.sin((entity.getYRot() / 180) * (float) Math.PI);
-						deltaY = -Math.sin((entity.getXRot() / 180) * (float) Math.PI);
-						deltaZ = Math.cos((entity.getYRot() / 180) * (float) Math.PI);
-						speed = 0.4;
-						motionX = deltaX * speed;
-						motionY = deltaY * speed;
-						motionZ = deltaZ * speed;
-						entity.setDeltaMovement(entity.getDeltaMovement().add(motionX, motionY, motionZ));
-						if (entity instanceof Player _player)
-							_player.causeFoodExhaustion((float) 0.5);
-					}
-					if (entity.isShiftKeyDown()) {
-						deltaX = -Math.sin((entity.getYRot() / 180) * (float) Math.PI);
-						deltaY = entity.level.clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(1)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, entity)).getBlockPos().getY()
-								- entity.getY();
-						deltaZ = Math.cos((entity.getYRot() / 180) * (float) Math.PI);
-						speed = 0.15;
-						Yspeed = 0.5;
-						motionX = deltaX * speed;
-						motionY = deltaY * Yspeed;
-						motionZ = deltaZ * speed;
-						entity.setDeltaMovement(entity.getDeltaMovement().add(motionX, motionY, motionZ));
-						if (entity instanceof Player _player)
-							_player.causeFoodExhaustion((float) (motionY * 0.25));
+				if ((entity instanceof Player _plr ? _plr.getFoodData().getFoodLevel() : 0) > 6) {
+					if (entity.isOnGround() && !entity.isInWater()) {
+						if (!entity.isShiftKeyDown()) {
+							deltaX = -Math.sin((entity.getYRot() / 180) * (float) Math.PI);
+							deltaY = -Math.sin((entity.getXRot() / 180) * (float) Math.PI);
+							deltaZ = Math.cos((entity.getYRot() / 180) * (float) Math.PI);
+							speed = 0.4;
+							motionX = deltaX * speed;
+							motionY = deltaY * speed;
+							motionZ = deltaZ * speed;
+							entity.setDeltaMovement(entity.getDeltaMovement().add(motionX, motionY, motionZ));
+							if (!(new Object() {
+								public boolean checkGamemode(Entity _ent) {
+									if (_ent instanceof ServerPlayer _serverPlayer) {
+										return _serverPlayer.gameMode.getGameModeForPlayer() == GameType.CREATIVE;
+									} else if (_ent.level.isClientSide() && _ent instanceof Player _player) {
+										return Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()) != null
+												&& Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()).getGameMode() == GameType.CREATIVE;
+									}
+									return false;
+								}
+							}.checkGamemode(entity))) {
+								if (entity instanceof Player _player)
+									_player.causeFoodExhaustion((float) 0.25);
+							}
+						}
+						if (entity.isShiftKeyDown()) {
+							deltaX = -Math.sin((entity.getYRot() / 180) * (float) Math.PI);
+							deltaY = entity.level.clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(1)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, entity)).getBlockPos().getY()
+									- entity.getY();
+							deltaZ = Math.cos((entity.getYRot() / 180) * (float) Math.PI);
+							speed = 0.15;
+							Yspeed = 0.5;
+							motionX = deltaX * speed;
+							motionY = deltaY * Yspeed;
+							motionZ = deltaZ * speed;
+							entity.setDeltaMovement(entity.getDeltaMovement().add(motionX, motionY, motionZ));
+							if (!(new Object() {
+								public boolean checkGamemode(Entity _ent) {
+									if (_ent instanceof ServerPlayer _serverPlayer) {
+										return _serverPlayer.gameMode.getGameModeForPlayer() == GameType.CREATIVE;
+									} else if (_ent.level.isClientSide() && _ent instanceof Player _player) {
+										return Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()) != null
+												&& Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()).getGameMode() == GameType.CREATIVE;
+									}
+									return false;
+								}
+							}.checkGamemode(entity))) {
+								if (entity instanceof Player _player)
+									_player.causeFoodExhaustion((float) (motionY * 0.25));
+							}
+						}
 					}
 				}
 			}
