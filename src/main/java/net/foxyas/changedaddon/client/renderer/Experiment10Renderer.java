ce0730a058b4da57149501ec3add1c60492fa552
Foxyas <CarlosDeck9474@gmail.com>
2024-04-29 00:08:04 -0300

    a few changes
    change to a female cat like armor (i am dumb)
    add the Exp10 Transfur to the catlike tag

--- src/main/java/net/foxyas/changedaddon/client/renderer/Experiment10Renderer.java
+++ src/main/java/net/foxyas/changedaddon/client/renderer/Experiment10Renderer.java
@@ -8,15 +8,15 @@ import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.EmissiveBodyLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.GasMaskLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleCatModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexFemaleCatModel;
 import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class Experiment10Renderer extends LatexHumanoidRenderer<Experiment10Entity, Experiment10Model, ArmorLatexMaleCatModel<Experiment10Entity>> {
+public class Experiment10Renderer extends LatexHumanoidRenderer<Experiment10Entity, Experiment10Model, ArmorLatexFemaleCatModel<Experiment10Entity>> {
 	public Experiment10Renderer(EntityRendererProvider.Context context) {
 		super(context, new Experiment10Model(context.bakeLayer(Experiment10Model.LAYER_LOCATION)),
-				ArmorLatexMaleCatModel::new, ArmorLatexMaleCatModel.INNER_ARMOR, ArmorLatexMaleCatModel.OUTER_ARMOR, 0.5f);
+				ArmorLatexFemaleCatModel::new, ArmorLatexFemaleCatModel.INNER_ARMOR, ArmorLatexFemaleCatModel.OUTER_ARMOR, 0.5f);
 		this.addLayer(new EmissiveBodyLayer<>(this, new ResourceLocation("changed_addon","textures/entities/exp-010_emissive.png")));
 		this.addLayer(new LatexParticlesLayer<>(this, getModel()));
 		this.addLayer(new GasMaskLayer<>(this, context.getModelSet()));
@@ -25,7 +25,7 @@ public class Experiment10Renderer extends LatexHumanoidRenderer<Experiment10Enti
 
 	@Override
 	public ResourceLocation getTextureLocation(Experiment10Entity entity) {
-		if (entity.isPhase2()) {
+		if (entity.getUnderlyingPlayer() == null && entity.isPhase2()) {
 			return new ResourceLocation("changed_addon:textures/entities/exp-10-phase2.png");
 		} else {
 			return new ResourceLocation("changed_addon:textures/entities/exp-10.png");
