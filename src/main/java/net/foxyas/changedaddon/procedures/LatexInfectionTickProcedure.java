a906333c1fb7c806cf5aa8eed89d14d29bbf5155
Foxyas <CarlosDeck9474@gmail.com>
2024-07-13 23:10:28 -0300

    Optimizade Tick update code wave 3

--- src/main/java/net/foxyas/changedaddon/procedures/LatexInfectionTickProcedure.java
+++ src/main/java/net/foxyas/changedaddon/procedures/LatexInfectionTickProcedure.java
@@ -7,11 +7,9 @@ import net.minecraftforge.event.TickEvent;
 
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.client.Minecraft;
 
 import net.foxyas.changedaddon.network.ChangedAddonModVariables;
@@ -36,12 +34,10 @@ public class LatexInfectionTickProcedure {
 		if (entity == null)
 			return;
 		double TransfurProgress = 0;
-		AttributeInstance attributeInstance = ((LivingEntity) entity).getAttribute(ChangedAddonModAttributes.LATEXINFECTION.get());
-		double LatexResistence_local_var = attributeInstance.getValue();
-		float TransfurProgress_local_var = (float) LatexResistence_local_var;
+		double TransfurProgress_local_var = 0;
 		TransfurProgress = (entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).Progress_Transfur_Number;
-		float PlayerTransfurProgress = (float) TransfurProgress;
-		float Math = PlayerTransfurProgress * TransfurProgress_local_var / 100;
+		TransfurProgress_local_var = ((LivingEntity) entity).getAttribute(ChangedAddonModAttributes.LATEXINFECTION.get()).getValue();
+		float Math = (float) (TransfurProgress * TransfurProgress_local_var / 100);
 		if (!(new Object() {
 			public boolean checkGamemode(Entity _ent) {
 				if (_ent instanceof ServerPlayer _serverPlayer) {
@@ -63,15 +59,9 @@ public class LatexInfectionTickProcedure {
 		}.checkGamemode(entity))) {
 			if (((LivingEntity) entity).getAttribute(ChangedAddonModAttributes.LATEXINFECTION.get()).getValue() > 0) {
 				if ((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).transfur == false) {
-					if (new Object() {
-						public double getValue() {
-							CompoundTag dataIndex3 = new CompoundTag();
-							entity.saveWithoutId(dataIndex3);
-							return dataIndex3.getDouble("TransfurProgress");
-						}
-					}.getValue() > 0) {
+					if (TransfurProgress > 0) {
 						if (entity.isAlive()) {
-							AddTransfurProgressProcedure.set(entity, PlayerTransfurProgress + Math);
+							AddTransfurProgressProcedure.set(entity, TransfurProgress + Math);
 						}
 					}
 				}
--- src/main/java/net/foxyas/changedaddon/procedures/TickHazardArmorFeatureProcedure.java
+++ src/main/java/net/foxyas/changedaddon/procedures/TickHazardArmorFeatureProcedure.java
@@ -1,26 +1,7 @@
 package net.foxyas.changedaddon.procedures;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.event.TickEvent;
-
-import javax.annotation.Nullable;
-
-@Mod.EventBusSubscriber
 public class TickHazardArmorFeatureProcedure {
-	@SubscribeEvent
-	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-		if (event.phase == TickEvent.Phase.END) {
-			execute(event);
-		}
-	}
-
 	public static void execute() {
-		execute(null);
-	}
-
-	private static void execute(@Nullable Event event) {
 		double LatexSolvent_level = 0;
 		boolean already_1 = false;
 	}
