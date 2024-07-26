fbda94461722621c3f03722e9ec5fa0d2ec551eb
Foxyas <CarlosDeck9474@gmail.com>
2024-07-26 00:10:19 -0300

    Update SizeManipulatorProcedure.java

--- src/main/java/net/foxyas/changedaddon/procedures/SizeManipulatorProcedure.java
+++ src/main/java/net/foxyas/changedaddon/procedures/SizeManipulatorProcedure.java
@@ -5,6 +5,7 @@ import com.mojang.brigadier.context.CommandContext;
 import net.foxyas.changedaddon.ChangedAddonMod;
 import net.ltxprogrammer.changed.Changed;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
@@ -21,19 +22,21 @@ public class SizeManipulatorProcedure {
 
 	public static float getSize(float size) {
 		if (size < 1.0f - SIZE_TOLERANCE) {
-			ChangedAddonMod.LOGGER.atWarn().log("Size value is too low: " + size + ",The Size Value gonna be Auto Set to 0.95"); // Too Low Warn
+			ChangedAddonMod.LOGGER.atWarn().log("Size value is too low: " + size + ", The Size Value is going to be auto set to 0.95"); // Too Low Warn
 		} else if (size > 1.0f + SIZE_TOLERANCE) {
-			ChangedAddonMod.LOGGER.atWarn().log("Size value is too high: " + size + ",The Size Value gonna be Auto Set to 1.05"); // Too High Warn
+			ChangedAddonMod.LOGGER.atWarn().log("Size value is too high: " + size + ", The Size Value is going to be auto set to 1.05"); // Too High Warn
 		}
 		return Mth.clamp(size, 1.0f - SIZE_TOLERANCE, 1.0f + SIZE_TOLERANCE);
 	}
 
 	public static void SizeChange(Entity entity, float amount) {
-		if (entity instanceof Player) {
-			Changed.config.client.basicPlayerInfo.setSize(getSize(amount)); //Change Size
-			ChangedAddonMod.LOGGER.info("Size changed to: " + getSize(amount) + " for player: " + entity.getName().getString()); //Command Classic Log
+		if (entity instanceof Player player) {
+			float newSize = getSize(amount);
+			Changed.config.client.basicPlayerInfo.setSize(newSize); // Change Size
+			ChangedAddonMod.LOGGER.info("Size changed to: " + newSize + " for player: " + player.getName().getString()); // Command Classic Log
+			player.displayClientMessage(Component.literal("Size changed to: " + newSize), false); // Chat log for the player
 		} else {
-			ChangedAddonMod.LOGGER.atError().log("Entity is not a player, cannot change size."); //Command Classic Error
+			ChangedAddonMod.LOGGER.atError().log("Entity is not a player, cannot change size."); // Command Classic Error
 		}
 	}
 }
