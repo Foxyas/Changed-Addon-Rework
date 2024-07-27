0e500b1655a26f56f5dfdbf296a16d0b490a77f4
Foxyas <CarlosDeck9474@gmail.com>
2024-07-27 13:20:48 -0300

    tweaks in some codes

--- src/main/java/net/foxyas/changedaddon/procedures/MusicPlayerProcedure.java
+++ src/main/java/net/foxyas/changedaddon/procedures/MusicPlayerProcedure.java
@@ -30,133 +30,138 @@ import net.foxyas.changedaddon.ChangedAddonMod;
 import javax.annotation.Nullable;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 @Mod.EventBusSubscriber
 public class MusicPlayerProcedure {
-	@SubscribeEvent
-	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-		if (event.phase == TickEvent.Phase.END) {
-			PlayBossMusic(event, event.player.level, event.player);
-		}
-	}
-	public static void PlayBossMusic(@Nullable Event event, LevelAccessor world, Entity entity) {
-		if (entity == null) {
-			return;
-		}
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            PlayBossMusic(event, event.player.level, event.player);
+        }
+    }
 
-		double x = entity.getX();
-		double y = entity.getY();
-		double z = entity.getZ();
+    public static void PlayBossMusic(@Nullable Event event, LevelAccessor world, Entity entity) {
+        if (entity == null) {
+            return;
+        }
 
-		Entity Exp10 = (Entity) world.getEntitiesOfClass(Experiment10Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).stream().sorted(new Object() {
-			Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
-				return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
-			}
-		}.compareDistOf(x, y, z)).findFirst().orElse(null);
-		//Entity Exp10
+        double x = entity.getX();
+        double y = entity.getY();
+        double z = entity.getZ();
 
-	    Entity Ket = (Entity) world.getEntitiesOfClass(KetExperiment009Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).stream().sorted(new Object() {
-			Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
-	  			return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
-			}
-		}.compareDistOf(x, y, z)).findFirst().orElse(null);
- 		//Entity Experiment009 KET
+        Entity Exp10 = world.getEntitiesOfClass(Experiment10Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).stream().sorted(new Object() {
+            Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+                return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
+            }
+        }.compareDistOf(x, y, z)).findFirst().orElse(null);
+        //Entity Exp10
 
-		Entity Experiment009 = (Entity) world.getEntitiesOfClass(Experiment009Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).stream().sorted(new Object() {
-			Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
-				return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
-			}
-		}.compareDistOf(x, y, z)).findFirst().orElse(null);
- 		//Entity Experiment009Phase1
+        Entity Ket = world.getEntitiesOfClass(KetExperiment009Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).stream().sorted(new Object() {
+            Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+                return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
+            }
+        }.compareDistOf(x, y, z)).findFirst().orElse(null);
+        //Entity Experiment009 KET
 
-		Entity Experiment009Phase2 = (Entity) world.getEntitiesOfClass(Experiment009phase2Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).stream().sorted(new Object() {
-			Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
-				return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
-			}
-		}.compareDistOf(x, y, z)).findFirst().orElse(null);
- 		//Entity Experiment009Phase2
+        Entity Experiment009 = world.getEntitiesOfClass(Experiment009Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).stream().sorted(new Object() {
+            Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+                return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
+            }
+        }.compareDistOf(x, y, z)).findFirst().orElse(null);
+        //Entity Experiment009Phase1
 
-		boolean Phase1isclose = !world.getEntitiesOfClass(Experiment009Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).isEmpty();
-		boolean Phase2isclose = !world.getEntitiesOfClass(Experiment009phase2Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).isEmpty();
-		boolean KetisClose = !world.getEntitiesOfClass(KetExperiment009Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).isEmpty();
-		boolean Exp10Close = !world.getEntitiesOfClass(Experiment10Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).isEmpty();
+        Entity Experiment009Phase2 = world.getEntitiesOfClass(Experiment009phase2Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).stream().sorted(new Object() {
+            Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+                return Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_x, _y, _z));
+            }
+        }.compareDistOf(x, y, z)).findFirst().orElse(null);
+        //Entity Experiment009Phase2
 
-
-		if (world.isClientSide() && ChangedAddonClientConfigsConfiguration.MUSICPLAYER.get() == true) {
-			boolean canwork = false;
-
-		Minecraft minecraft = Minecraft.getInstance();
-			MusicManager musicManager = minecraft.getMusicManager();
-
-			boolean Spectator = new Object() {
-				public boolean checkGamemode(Player _player) {
-					if (_player instanceof ServerPlayer _serverPlayer) {
-						return _serverPlayer.gameMode.getGameModeForPlayer() == GameType.SPECTATOR;
-					} else if (_player.level.isClientSide()) {
-						return Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()) != null
-								&& Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()).getGameMode() == GameType.SPECTATOR;
-					}
-					return false;
-				}
-			}.checkGamemode((Player) entity);
+        boolean Exp009Phase1IsClose = !world.getEntitiesOfClass(Experiment009Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).isEmpty();
+        boolean Exp009Phase2IsClose = !world.getEntitiesOfClass(Experiment009phase2Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).isEmpty();
+        boolean KetExp9IsClose = !world.getEntitiesOfClass(KetExperiment009Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).isEmpty();
+        boolean Exp10Close = !world.getEntitiesOfClass(Experiment10Entity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).isEmpty();
 
 
-			if (!Spectator) {
-				canwork = true;
-			}
-				net.minecraft.sounds.SoundEvent Experiment009Phase2Music = ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(ChangedAddonMod.MODID, "experiment009_theme_phase2"));
-				Music Experiment009_phase2_theme_MusicInstance = new Music(Experiment009Phase2Music, 0, 0, true);
-				boolean isExperiment009Phase2ThemePlaying = musicManager.isPlayingMusic(Experiment009_phase2_theme_MusicInstance);
-				net.minecraft.sounds.SoundEvent Experiment009Music = ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(ChangedAddonMod.MODID, "experiment009_theme"));
-				Music Experiment009_Theme_MusicInstance = new Music(Experiment009Music, 0, 0, true);
-				boolean isExperiment009ThemePlaying = musicManager.isPlayingMusic(Experiment009_Theme_MusicInstance);
-				net.minecraft.sounds.SoundEvent Experiment10Music = ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(ChangedAddonMod.MODID, "experiment10_theme"));
-				Music Experiment10_Theme_MusicInstance = new Music(Experiment10Music, 0, 0, true);
-				boolean isExperiment10ThemePlaying = musicManager.isPlayingMusic(Experiment10_Theme_MusicInstance);
+        if (world.isClientSide() && ChangedAddonClientConfigsConfiguration.MUSICPLAYER.get()) {
+            boolean canwork = false;
+
+            Minecraft minecraft = Minecraft.getInstance();
+            MusicManager musicManager = minecraft.getMusicManager();
+
+            boolean Spectator = new Object() {
+                public boolean checkGameMode(Player _player) {
+                    if (_player instanceof ServerPlayer _serverPlayer) {
+                        return _serverPlayer.gameMode.getGameModeForPlayer() == GameType.SPECTATOR;
+                    } else if (_player.level.isClientSide()) {
+                        return Objects.requireNonNull(Minecraft.getInstance().getConnection()).getPlayerInfo(_player.getGameProfile().getId()) != null
+                                && Objects.requireNonNull(Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId())).getGameMode() == GameType.SPECTATOR;
+                    }
+                    return false;
+                }
+            }.checkGameMode((Player) entity);
 
 
-			if (Phase2isclose || KetisClose && canwork) {
-				if (KetisClose && !isExperiment009Phase2ThemePlaying) {
-					musicManager.startPlaying(Experiment009_phase2_theme_MusicInstance);
-				}
-				if (!isExperiment009Phase2ThemePlaying) {
-					musicManager.startPlaying(Experiment009_phase2_theme_MusicInstance);
-				} else if (Experiment009Phase2 != null && !Experiment009Phase2.isAlive() ) {
-					if (isExperiment009Phase2ThemePlaying){
-						minecraft.getSoundManager().stop(new ResourceLocation("changed_addon","experiment009_theme_phase2"), SoundSource.MUSIC);
-					}
-				}  if (Ket != null && !Ket.isAlive() ) {
-					if (isExperiment009Phase2ThemePlaying){
-						minecraft.getSoundManager().stop(new ResourceLocation("changed_addon","experiment009_theme_phase2"), SoundSource.MUSIC);
-							}
-						} //If the 009 KET is not alive
-					} else if (!Phase2isclose && !KetisClose && isExperiment009Phase2ThemePlaying){
-					minecraft.getSoundManager().stop(new ResourceLocation("changed_addon","experiment009_theme_phase2"), SoundSource.MUSIC);
-				}
- 			else if (Phase1isclose && canwork) {
-				if (!isExperiment009ThemePlaying) {
-					musicManager.startPlaying(Experiment009_Theme_MusicInstance);
-				} else if (isExperiment009ThemePlaying && Experiment009 != null){
-					if (!Experiment009.isAlive()){
-						minecraft.getSoundManager().stop(new ResourceLocation("changed_addon","experiment009_theme"), SoundSource.MUSIC);
-					}
-				}} else if (!Phase1isclose && isExperiment009ThemePlaying){
-						minecraft.getSoundManager().stop(new ResourceLocation("changed_addon","experiment009_theme"), SoundSource.MUSIC);
-					}
-				else if (Exp10Close && canwork){
-				if (!isExperiment10ThemePlaying) {
-					musicManager.startPlaying(Experiment10_Theme_MusicInstance);
-				} else if (isExperiment10ThemePlaying && Exp10 != null){
-					if (!Exp10.isAlive()){
-						minecraft.getSoundManager().stop(new ResourceLocation("changed_addon","experiment10_theme"), SoundSource.MUSIC);
-					}
-				}} else if (!Exp10Close && isExperiment10ThemePlaying){
-				minecraft.getSoundManager().stop(new ResourceLocation("changed_addon","experiment10_theme"), SoundSource.MUSIC);
-					}
+            if (!Spectator) {
+                canwork = true;
+            }
 
-			} //All before that is in client side
+            //Sound Events
+            net.minecraft.sounds.SoundEvent Experiment009Phase2Music = ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(ChangedAddonMod.MODID, "experiment009_theme_phase2"));
+            net.minecraft.sounds.SoundEvent Experiment009Music = ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(ChangedAddonMod.MODID, "experiment009_theme"));
+            net.minecraft.sounds.SoundEvent Experiment10Music = ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(ChangedAddonMod.MODID, "experiment10_theme"));
 
-		}
+            //Music Creator
+            Music Experiment009_Theme_MusicInstance = new Music(Objects.requireNonNull(Experiment009Music), 0, 0, true);
+            Music Experiment10_Theme_MusicInstance = new Music(Objects.requireNonNull(Experiment10Music), 0, 0, true);
+            Music Experiment009_phase2_theme_MusicInstance = new Music(Objects.requireNonNull(Experiment009Phase2Music), 0, 0, true);
 
- //End of the Void
+            //Check if theme is playing
+            boolean isExperiment009ThemePlaying = musicManager.isPlayingMusic(Experiment009_Theme_MusicInstance);
+            boolean isExperiment10ThemePlaying = musicManager.isPlayingMusic(Experiment10_Theme_MusicInstance);
+            boolean isExperiment009Phase2ThemePlaying = musicManager.isPlayingMusic(Experiment009_phase2_theme_MusicInstance);
+
+            if (Exp009Phase2IsClose || KetExp9IsClose && canwork) {
+                if (KetExp9IsClose && !isExperiment009Phase2ThemePlaying) {
+                    musicManager.startPlaying(Experiment009_phase2_theme_MusicInstance);
+                }
+                if (!isExperiment009Phase2ThemePlaying) {
+                    musicManager.startPlaying(Experiment009_phase2_theme_MusicInstance);
+                } else if (Experiment009Phase2 != null && !Experiment009Phase2.isAlive()) {
+                    if (isExperiment009Phase2ThemePlaying) {
+                        minecraft.getSoundManager().stop(new ResourceLocation("changed_addon", "experiment009_theme_phase2"), SoundSource.MUSIC);
+                    }
+                }
+                if (Ket != null && !Ket.isAlive()) {
+                    if (isExperiment009Phase2ThemePlaying) {
+                        minecraft.getSoundManager().stop(new ResourceLocation("changed_addon", "experiment009_theme_phase2"), SoundSource.MUSIC);
+                    }
+                } //If the 009 KET is not alive
+            } else if (!Exp009Phase2IsClose && !KetExp9IsClose && isExperiment009Phase2ThemePlaying) {
+                minecraft.getSoundManager().stop(new ResourceLocation("changed_addon", "experiment009_theme_phase2"), SoundSource.MUSIC);
+            } else if (Exp009Phase1IsClose && canwork) {
+                if (!isExperiment009ThemePlaying) {
+                    musicManager.startPlaying(Experiment009_Theme_MusicInstance);
+                } else if (isExperiment009ThemePlaying && Experiment009 != null) {
+                    if (!Experiment009.isAlive()) {
+                        minecraft.getSoundManager().stop(new ResourceLocation("changed_addon", "experiment009_theme"), SoundSource.MUSIC);
+                    }
+                }
+            } else if (!Exp009Phase1IsClose && isExperiment009ThemePlaying) {
+                minecraft.getSoundManager().stop(new ResourceLocation("changed_addon", "experiment009_theme"), SoundSource.MUSIC);
+            } else if (Exp10Close && canwork) {
+                if (!isExperiment10ThemePlaying) {
+                    musicManager.startPlaying(Experiment10_Theme_MusicInstance);
+                } else if (isExperiment10ThemePlaying && Exp10 != null) {
+                    if (!Exp10.isAlive()) {
+                        minecraft.getSoundManager().stop(new ResourceLocation("changed_addon", "experiment10_theme"), SoundSource.MUSIC);
+                    }
+                }
+            } else if (!Exp10Close && isExperiment10ThemePlaying) {
+                minecraft.getSoundManager().stop(new ResourceLocation("changed_addon", "experiment10_theme"), SoundSource.MUSIC);
+            }
+
+        } // <- This end the client side part,All before that is in client side
+    } //End of the Void
 }
--- src/main/java/net/foxyas/changedaddon/procedures/SignalBlockFeatureProcedure.java
+++ src/main/java/net/foxyas/changedaddon/procedures/SignalBlockFeatureProcedure.java
@@ -20,103 +20,107 @@ import java.util.Objects;
 
 public class SignalBlockFeatureProcedure {
 
-    private static final int LARGE_SEARCH_RADIUS = 128;
-    private static final int SMALL_SEARCH_RADIUS = 32;
-    private static final int MAX_FOUND_BLOCKS = 10;
+	private static final int LARGE_SEARCH_RADIUS = 128;
+	private static final int SMALL_SEARCH_RADIUS = 32;
+	private static final int MAX_FOUND_BLOCKS = 10;
 
-    public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack itemstack) {
-        if (entity == null) return;
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack itemstack) {
+		if (entity == null)
+			return;
 
-        if (isSignalCatcherInHand(entity)) {
-            Player player = (Player) entity;
-            if (!player.getCooldowns().isOnCooldown(ChangedAddonModItems.SIGNAL_CATCHER.get())) {
-                int radius = player.isShiftKeyDown() ? LARGE_SEARCH_RADIUS : SMALL_SEARCH_RADIUS;
-                int cooldown = player.isShiftKeyDown() ? 100 : 60;
-                searchSignalBlock(world, x, y, z, player, itemstack, radius, cooldown);
-            }
-        }
-    }
+		if (isSignalCatcherInHand(entity)) {
+			Player player = (Player) entity;
+			if (!player.getCooldowns().isOnCooldown(ChangedAddonModItems.SIGNAL_CATCHER.get())) {
+				int radius = player.isShiftKeyDown() ? LARGE_SEARCH_RADIUS : SMALL_SEARCH_RADIUS;
+				int cooldown = player.isShiftKeyDown() ? 100 : 60;
+				searchSignalBlock(world, x, y, z, player, itemstack, radius, cooldown);
+			}
+		}
+	}
 
-    private static boolean isSignalCatcherInHand(Entity entity) {
-        LivingEntity livingEntity = (LivingEntity) entity;
-        return livingEntity.getMainHandItem().getItem() == ChangedAddonModItems.SIGNAL_CATCHER.get() ||
-               livingEntity.getOffhandItem().getItem() == ChangedAddonModItems.SIGNAL_CATCHER.get();
-    }
+	private static boolean isSignalCatcherInHand(Entity entity) {
+		LivingEntity livingEntity = (LivingEntity) entity;
+		return livingEntity.getMainHandItem().getItem() == ChangedAddonModItems.SIGNAL_CATCHER.get() || livingEntity.getOffhandItem().getItem() == ChangedAddonModItems.SIGNAL_CATCHER.get();
+	}
 
-    private static void searchSignalBlock(LevelAccessor world, double x, double y, double z, Player player, ItemStack itemstack, int radius, int cooldown) {
-        int startX = (int) x - radius;
-        int endX = (int) x + radius;
-        int startY = (int) y - radius;
-        int endY = (int) y + radius;
-        int startZ = (int) z - radius;
-        int endZ = (int) z + radius;
+	private static void searchSignalBlock(LevelAccessor world, double x, double y, double z, Player player, ItemStack itemstack, int radius, int cooldown) {
+		int startX = (int) x - radius;
+		int endX = (int) x + radius;
+		int startY = (int) y - radius;
+		int endY = (int) y + radius;
+		int startZ = (int) z - radius;
+		int endZ = (int) z + radius;
 
-        List<BlockPos> foundPositions = new ArrayList<>();
-        
-        outerLoop:
-        for (int sx = startX; sx <= endX; sx++) {
-            for (int sy = startY; sy <= endY; sy++) {
-                for (int sz = startZ; sz <= endZ; sz++) {
-                    BlockPos currentPos = new BlockPos(sx, sy, sz);
-                    if (world.getBlockState(currentPos).getBlock() == ChangedAddonModBlocks.SIGNAL_BLOCK.get()) {
-                        foundPositions.add(currentPos);
-                        // Only update player state for the first found block
-                        if (foundPositions.size() == 1) {
-                            updatePlayerState(player, itemstack, sx, sy, sz, cooldown);
-                        }
-                        // Break loop if the number of found blocks exceeds MAX_FOUND_BLOCKS
-                        if (foundPositions.size() > MAX_FOUND_BLOCKS) {
-                            break outerLoop;
-                        }
-                    }
-                }
-            }
-        }
+		List<BlockPos> foundPositions = new ArrayList<>();
 
-        if (!foundPositions.isEmpty()) {
-            displayFoundLocations(player, foundPositions);
-            playSounds(world, x, y, z, foundPositions.get(0));
-        } else if (!player.level.isClientSide()) {
-            player.displayClientMessage(new TextComponent("No Signal Block Found"), false);
-        }
-    }
+		if (!world.isClientSide()) {
+			for (int sx = startX; sx <= endX; sx++) {
+				for (int sy = startY; sy <= endY; sy++) {
+					for (int sz = startZ; sz <= endZ; sz++) {
+						BlockPos currentPos = new BlockPos(sx, sy, sz);
+						if (world.getBlockState(currentPos).getBlock() == ChangedAddonModBlocks.SIGNAL_BLOCK.get()) {
+							foundPositions.add(currentPos);
+							// Only update player state for the first found block
+							if (foundPositions.size() == 1) {
+								updatePlayerState(player, itemstack, sx, sy, sz, cooldown);
+							}
+							// Break loop if the number of found blocks exceeds MAX_FOUND_BLOCKS
+							if (foundPositions.size() >= MAX_FOUND_BLOCKS) {
+								break;
+							}
+						}
+					}
+					if (foundPositions.size() == MAX_FOUND_BLOCKS) {
+						break;
+					}
+				}
+				if (foundPositions.size() == MAX_FOUND_BLOCKS) {
+					break;
+				}
+			}
+		}
 
-    private static void updatePlayerState(Player player, ItemStack itemstack, int x, int y, int z, int cooldown) {
-        itemstack.getOrCreateTag().putDouble("x", x);
-        itemstack.getOrCreateTag().putDouble("y", y);
-        itemstack.getOrCreateTag().putDouble("z", z);
-        itemstack.getOrCreateTag().putBoolean("set", true);
-        player.getCooldowns().addCooldown(itemstack.getItem(), cooldown);
-    }
+		if (!foundPositions.isEmpty()) {
+			displayFoundLocations(player, foundPositions);
+			playSounds(world, x, y, z, foundPositions.get(0));
+		} else if (!player.level.isClientSide()) {
+			player.displayClientMessage(new TextComponent("No Signal Block Found"), false);
+		}
+	}
 
-    private static void displayFoundLocations(Player player, List<BlockPos> positions) {
-        StringBuilder message = new StringBuilder("Signal Blocks found at:");
-        for (int i = 0; i < positions.size(); i++) {
-            BlockPos pos = positions.get(i);
-            message.append(" Block ").append(i + 1).append(": [")
-                   .append(pos.getX()).append(", ")
-                   .append(pos.getY()).append(", ")
-                   .append(pos.getZ()).append("]");
-            if (i < positions.size() - 1) {
-                message.append("; ");
-            }
-        }
-        player.displayClientMessage(new TextComponent(message.toString()), false);
-    }
+	private static void updatePlayerState(Player player, ItemStack itemstack, int x, int y, int z, int cooldown) {
+		itemstack.getOrCreateTag().putDouble("x", x);
+		itemstack.getOrCreateTag().putDouble("y", y);
+		itemstack.getOrCreateTag().putDouble("z", z);
+		itemstack.getOrCreateTag().putBoolean("set", true);
+		player.getCooldowns().addCooldown(itemstack.getItem(), cooldown);
+	}
 
-    private static void playSounds(LevelAccessor world, double x, double y, double z, BlockPos foundPos) {
-        playSound(world, foundPos, "block.conduit.deactivate", SoundSource.BLOCKS, 1.5f);
-        playSound(world, new BlockPos(x, y, z), "block.conduit.activate", SoundSource.PLAYERS, 1.5f);
-    }
+	private static void displayFoundLocations(Player player, List<BlockPos> positions) {
+		StringBuilder message = new StringBuilder("Signal Blocks found at:\n");
+		for (int i = 0; i < positions.size(); i++) {
+			BlockPos pos = positions.get(i);
+			message.append("Block ").append(i + 1).append(": [").append(pos.getX()).append(", ").append(pos.getY()).append(", ").append(pos.getZ()).append("]\n");
+			if (i < positions.size() - 1) {
+				message.append("; ");
+			}
+		}
+		player.displayClientMessage(new TextComponent(message.toString()), false);
+	}
 
-    private static void playSound(LevelAccessor world, BlockPos pos, String soundEvent, SoundSource source, float volume) {
-        if (world instanceof Level level) {
-            ResourceLocation soundLocation = new ResourceLocation(soundEvent);
-            if (!level.isClientSide()) {
-                level.playSound(null, pos, Objects.requireNonNull(ForgeRegistries.SOUND_EVENTS.getValue(soundLocation)), source, volume, 0);
-            } else {
-                level.playLocalSound(pos.getX(), pos.getY(), pos.getZ(), Objects.requireNonNull(ForgeRegistries.SOUND_EVENTS.getValue(soundLocation)), source, volume, 0, false);
-            }
-        }
-    }
+	private static void playSounds(LevelAccessor world, double x, double y, double z, BlockPos foundPos) {
+		playSound(world, foundPos, "block.conduit.deactivate", SoundSource.BLOCKS, 1.5f);
+		playSound(world, new BlockPos(x, y, z), "block.conduit.activate", SoundSource.PLAYERS, 1.5f);
+	}
+
+	private static void playSound(LevelAccessor world, BlockPos pos, String soundEvent, SoundSource source, float volume) {
+		if (world instanceof Level level) {
+			ResourceLocation soundLocation = new ResourceLocation(soundEvent);
+			if (!level.isClientSide()) {
+				level.playSound(null, pos, Objects.requireNonNull(ForgeRegistries.SOUND_EVENTS.getValue(soundLocation)), source, volume, 0);
+			} else {
+				level.playLocalSound(pos.getX(), pos.getY(), pos.getZ(), Objects.requireNonNull(ForgeRegistries.SOUND_EVENTS.getValue(soundLocation)), source, volume, 0, false);
+			}
+		}
+	}
 }
