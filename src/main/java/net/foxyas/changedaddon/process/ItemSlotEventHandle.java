c8ec1844e3e9efc0de3d82a9282a4103ec174a14
Foxyas <CarlosDeck9474@gmail.com>
2024-09-03 13:51:22 -0300

    tweak in code

--- src/main/java/net/foxyas/changedaddon/process/ItemSlotEventHandle.java
+++ src/main/java/net/foxyas/changedaddon/process/ItemSlotEventHandle.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class ItemSlotEventHandle {
+    /*
     @SubscribeEvent
     public static void onArmorEquip(LivingEquipmentChangeEvent event) {
         if (event.getEntity() instanceof Player player) {
@@ -31,6 +32,7 @@ public class ItemSlotEventHandle {
             }
         }
     }
+    */
 
     @SubscribeEvent
     public static void onPlayerRightClick(PlayerInteractEvent.RightClickItem event) {
