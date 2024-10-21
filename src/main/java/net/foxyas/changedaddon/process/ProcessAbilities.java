4e61ceca03e4cbd464bcf25adc14d333dfd08cc3
Foxyas <CarlosDeck9474@gmail.com>
2024-10-21 00:36:03 -0300

    add DodgeAbility Handle

--- src/main/java/net/foxyas/changedaddon/process/ProcessAbilities.java
+++ src/main/java/net/foxyas/changedaddon/process/ProcessAbilities.java
@@ -5,6 +5,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.TickEvent;
 
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.network.chat.TextComponent;
 
 import net.ltxprogrammer.changed.process.ProcessTransfur;
@@ -16,5 +17,37 @@ import net.foxyas.changedaddon.ability.ChangedAddonAbilitys;
 
 @Mod.EventBusSubscriber
 public class ProcessAbilities {
-    //TODO Place new Ability Handle here
+	//TODO Place new Ability Handle here
+	@Mod.EventBusSubscriber
+	public static class DodgeAbility {
+		@SubscribeEvent
+		public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+			Player player = event.player;
+			TransfurVariantInstance<?> TransfurVariantInstance = ProcessTransfur.getPlayerTransfurVariant(player);
+			if (TransfurVariantInstance == null) {
+				return;
+			}
+			DodgeAbilityInstance DodgeAbility = (DodgeAbilityInstance) TransfurVariantInstance.abilityInstances.get(ChangedAddonAbilitys.DODGE.get());
+			if (DodgeAbility == null) {
+				return;
+			}
+			int DodgeAmount = DodgeAbility.getDodgeAmount();
+			int MaxDodgeAmount = DodgeAbility.getMaxDodgeAmount();
+			boolean IsDodgeActivate = DodgeAbility.isDodgeActivate();
+			player.displayClientMessage(new TextComponent("TEXT12"), false);
+			if (!IsDodgeActivate) {
+				if (DodgeAmount < MaxDodgeAmount) {
+					if (DodgeRegenCooldown < 0) {
+						DodgeAmount++;
+						DodgeRegenCooldown = 5;
+						if (entity.getEntity() instanceof Player player) {
+							player.displayClientMessage(new TranslatableComponent("changed_addon.ability.dodge.dodge_amount", +DodgeAmount), true);
+						}
+					} else {
+						DodgeRegenCooldown--;
+					}
+				}
+			}
+		}
+	}
 }
