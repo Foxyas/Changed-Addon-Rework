2e078baddea7deb791a286a53fecdfab96d279c2
Foxyas <CarlosDeck9474@gmail.com>
2024-11-23 18:49:19 -0300

    Update ChangedEntityTargetSelectorMixin.java

--- src/main/java/net/foxyas/changedaddon/mixins/ChangedEntityTargetSelectorMixin.java
+++ src/main/java/net/foxyas/changedaddon/mixins/ChangedEntityTargetSelectorMixin.java
@@ -12,19 +12,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ChangedEntity.class)
+@Mixin(value = ChangedEntity.class,remap = false)
 public class ChangedEntityTargetSelectorMixin {
 
-    @Inject(method = "targetSelectorTest", at = @At("HEAD"))
+    @Inject(method = "targetSelectorTest", at = @At("HEAD"), cancellable = true)
     private void CancelTarget(LivingEntity livingEntity, CallbackInfoReturnable<Boolean> cir){
         ItemStack Head = livingEntity.getItemBySlot(EquipmentSlot.HEAD);
         ItemStack Chest = livingEntity.getItemBySlot(EquipmentSlot.CHEST);
         if (isDarkLatexCoat(Head) && isDarkLatexCoat(Chest)){
-            ChangedAddonMod.LOGGER.info("Evento cancelado: capacete e peitoral detectados");
+            //ChangedAddonMod.LOGGER.info("Evento cancelado: capacete e peitoral detectados");
             cir.setReturnValue(false);
         } else if (isDarkLatexCoat(Head) ^ isDarkLatexCoat(Chest)) {
             if (livingEntity.distanceTo((ChangedEntity) (Object) this) >= 4){
-                ChangedAddonMod.LOGGER.info("Evento cancelado: item parcial detectado, distância > 4");
+                //ChangedAddonMod.LOGGER.info("Evento cancelado: item parcial detectado, distância > 4");
                 cir.setReturnValue(false);
             }
         }
