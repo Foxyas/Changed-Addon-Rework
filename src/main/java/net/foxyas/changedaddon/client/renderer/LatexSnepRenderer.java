9f1cbe7fd10ddc0c9c9a2d096042d39789a9bf1f
Foxyas <CarlosDeck9474@gmail.com>
2024-12-05 17:23:19 -0300

    new Latex Snew Eyes and removed useless code

--- src/main/java/net/foxyas/changedaddon/client/renderer/LatexSnepRenderer.java
+++ src/main/java/net/foxyas/changedaddon/client/renderer/LatexSnepRenderer.java
@@ -4,14 +4,10 @@ package net.foxyas.changedaddon.client.renderer;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.foxyas.changedaddon.client.model.LatexSnepModel;
-import net.foxyas.changedaddon.client.model.ModelMirrorWhiteTiger;
-import net.foxyas.changedaddon.entity.KetExperiment009BossEntity;
 import net.ltxprogrammer.changed.client.FormRenderHandler;
 import net.ltxprogrammer.changed.client.renderer.AdvancedHumanoidRenderer;
-import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.FirstPersonLayer;
 import net.ltxprogrammer.changed.client.renderer.model.AdvancedHumanoidModel;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexFemaleCatModel;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorNoneModel;
 import net.ltxprogrammer.changed.entity.ChangedEntity;
 import net.minecraft.client.model.EntityModel;
@@ -22,17 +18,16 @@ import net.minecraft.client.renderer.entity.layers.EyesLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.model.geom.ModelLayers;
-import net.minecraft.client.model.HumanoidModel;
 
 import net.foxyas.changedaddon.entity.LatexSnepEntity;
 import net.minecraft.world.entity.HumanoidArm;
+import org.jetbrains.annotations.NotNull;
 
 public class LatexSnepRenderer extends AdvancedHumanoidRenderer<LatexSnepEntity, LatexSnepModel, ArmorNoneModel<LatexSnepEntity>> {
 	public LatexSnepRenderer(EntityRendererProvider.Context context) {
 		super(context, new LatexSnepModel(context.bakeLayer(LatexSnepModel.LAYER_LOCATION)),
 				ArmorNoneModel::new, ArmorNoneModel.INNER_ARMOR, ArmorNoneModel.OUTER_ARMOR, 0.5f);
-		this.addLayer(new CustomCatEyesLayer<>(this, new ResourceLocation("changed_addon:textures/entities/latex_snep_eyes.png")));
+		this.addLayer(new CustomCatEyesLayer<>(this, new ResourceLocation("changed_addon:textures/entities/latex_snep_eyes.png") , new ResourceLocation("changed_addon:textures/entities/latex_snep_sclera.png")));
 	}
 
 	@Override
@@ -41,27 +36,42 @@ public class LatexSnepRenderer extends AdvancedHumanoidRenderer<LatexSnepEntity,
 	}
 
 	private static class CustomCatEyesLayer<M extends EntityModel<T>, T extends ChangedEntity> extends EyesLayer<T, M> implements FirstPersonLayer<T> {
-		private final RenderType renderType;
-		private final ResourceLocation emissiveTexture;
+		private final RenderType EyesRenderType;
+		private final ResourceLocation eyesTexture;
+		private final RenderType ScleraRenderType;
+		private final ResourceLocation scleraTexture;
 
-		public CustomCatEyesLayer(RenderLayerParent<T, M> p_116964_, ResourceLocation emissiveTexture) {
+		public CustomCatEyesLayer(RenderLayerParent<T, M> p_116964_, ResourceLocation eyesTexture, ResourceLocation scleraTexture) {
 			super(p_116964_);
-			this.renderType = RenderType.eyes(emissiveTexture);
-			this.emissiveTexture = emissiveTexture;
+			this.EyesRenderType = RenderType.eyes(eyesTexture);
+			this.eyesTexture = eyesTexture;
+			this.ScleraRenderType = RenderType.eyes(scleraTexture);
+			this.scleraTexture = scleraTexture;
 		}
-		public ResourceLocation getEmissiveTexture() {
-			return this.emissiveTexture;
+		public ResourceLocation getTexture(int i) {
+			return i == 1 ? this.eyesTexture : this.scleraTexture;
 		}
 
 		@Override
 		public void render(PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-			VertexConsumer vertexConsumer = bufferSource.getBuffer(this.renderType());
+			VertexConsumer vertexConsumer = bufferSource.getBuffer(this.EyesRenderType);
 			this.getParentModel().renderToBuffer(poseStack, vertexConsumer, 15728640, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+			VertexConsumer vertexConsumer2 = bufferSource.getBuffer(this.ScleraRenderType);
+			this.getParentModel().renderToBuffer(poseStack, vertexConsumer2, 15728640, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
 		}
 
 
-		public RenderType renderType() {
-			return this.renderType;
+		@Override
+		public @NotNull RenderType renderType() {
+			return this.EyesRenderType;
+		}
+
+		public RenderType getEyesRenderType() {
+			return EyesRenderType;
+		}
+
+		public RenderType getScleraRenderType() {
+			return ScleraRenderType;
 		}
 
 		public void renderFirstPersonOnArms(PoseStack stack, MultiBufferSource bufferSource, int packedLight, T entity, HumanoidArm arm, PoseStack stackCorrector) {
@@ -69,7 +79,8 @@ public class LatexSnepRenderer extends AdvancedHumanoidRenderer<LatexSnepEntity,
 			stack.scale(1.0002F, 1.0002F, 1.0002F);
 			EntityModel<T> var8 = this.getParentModel();
 			if (var8 instanceof AdvancedHumanoidModel<?> armedModel) {
-				FormRenderHandler.renderModelPartWithTexture(armedModel.getArm(arm), stackCorrector, stack, bufferSource.getBuffer(this.renderType()), 15728880, 1.0F);
+				FormRenderHandler.renderModelPartWithTexture(armedModel.getArm(arm), stackCorrector, stack, bufferSource.getBuffer(this.EyesRenderType), 15728880, 1.0F);
+				FormRenderHandler.renderModelPartWithTexture(armedModel.getArm(arm), stackCorrector, stack, bufferSource.getBuffer(this.ScleraRenderType), 15728880, 1.0F);
 			}
 			stack.popPose();
 		}
