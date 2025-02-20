9baff133f377cebd26541523d4d146d44c317701
Foxyas <CarlosDeck9474@gmail.com>
2025-02-20 16:48:43 -0300

    Update PlayerUtilProcedure.java

--- src/main/java/net/foxyas/changedaddon/procedures/PlayerUtilProcedure.java
+++ src/main/java/net/foxyas/changedaddon/procedures/PlayerUtilProcedure.java
@@ -34,6 +34,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.phys.*;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
