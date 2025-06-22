59f41fda52ae7c24c29d0e63ca47c442de664689
Foxyas <CarlosDeck9474@gmail.com>
2025-06-22 00:37:34 -0300

    Update FormDietEvent.java

--- src/main/java/net/foxyas/changedaddon/process/variantsExtraStats/FormDietEvent.java
+++ src/main/java/net/foxyas/changedaddon/process/variantsExtraStats/FormDietEvent.java
@@ -14,6 +14,13 @@ public class FormDietEvent extends Event {
     public int additionalFood;
     public float additionalSaturation;
 
+    /**
+     * @param additionalFood is the additionalFood apply AFTER the player consume the food
+     * @param additionalSaturation is the additionalSaturation apply AFTER the player consume the food
+     *                             by default both values are -> (foodData.values/4)
+     *                             change this value as you please but remember that this is the exactly value and not an additional
+     *                             so this means that -> (additionalFood = 4) is a truly +4 of food level and not a (defaultValue + 4)
+     */
     public FormDietEvent(TransfurVariant<?> variant, LivingEntity entity, boolean isGoodFood, ItemStack itemStack, int additionalFood, float additionalSaturation){
         this.variant = variant;
         this.entity = entity;
