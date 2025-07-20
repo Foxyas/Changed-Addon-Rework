946a36eb4992ffaaa46c617e1075e85f5a7f115e
Foxyas <CarlosDeck9474@gmail.com>
2025-07-20 17:29:39 -0300

    Update ReturnTypeTransfurSoundProcedure.java

--- src/main/java/net/foxyas/changedaddon/procedures/ReturnTypeTransfurSoundProcedure.java
+++ src/main/java/net/foxyas/changedaddon/procedures/ReturnTypeTransfurSoundProcedure.java
@@ -1,29 +1,38 @@
 package net.foxyas.changedaddon.procedures;
 
-import net.foxyas.changedaddon.network.ChangedAddonModVariables;
+import net.ltxprogrammer.changed.entity.variant.TransfurVariant;
+import net.ltxprogrammer.changed.entity.variant.TransfurVariantInstance;
+import net.ltxprogrammer.changed.process.ProcessTransfur;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 
 public class ReturnTypeTransfurSoundProcedure {
     public static String execute(Entity entity) {
-        if (entity == null)
+        if (!(entity instanceof Player player))
             return "";
-        if ((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).transfur) {
-            if (((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).LatexForm).contains("cat")
-                    || ((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).LatexForm).contains("leopard")
-                    || ((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).LatexForm).contains("lynx")
-                    || ((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).LatexForm).contains("tiger")
-                    || ((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).LatexForm).equals("changed:form_latex_snow_leopard/female")
-                    || ((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).LatexForm).equals("changed:form_latex_snow_leopard/male")
-                    || ((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).LatexForm).equals("changed:form_latex_medusa_cat")
-                    || ((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).LatexForm).equals("changed:form_latex_stiger")
-                    || ((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).LatexForm).equals("changed:form_latex_white_tiger")) {
-                return "\u00A7fYou are a Cat Transfur";
-            }
-            if (((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).LatexForm).contains("wolf")
-                    || ((entity.getCapability(ChangedAddonModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ChangedAddonModVariables.PlayerVariables())).LatexForm).contains("dog")) {
-                return "\u00A7fYou are a Dog Transfur";
-            }
+
+        TransfurVariantInstance<?> variant = ProcessTransfur.getPlayerTransfurVariant(player);
+        if (variant == null)
+            return "§fYou are a Transfur";
+
+        String formId = variant.getFormId().toString();
+
+        if (formId.contains("cat") ||
+                formId.contains("leopard") ||
+                formId.contains("lynx") ||
+                formId.contains("tiger") ||
+                formId.equals("changed:form_latex_snow_leopard/female") ||
+                formId.equals("changed:form_latex_snow_leopard/male") ||
+                formId.equals("changed:form_latex_medusa_cat") ||
+                formId.equals("changed:form_latex_stiger") ||
+                formId.equals("changed:form_latex_white_tiger")) {
+            return "§fYou are a Cat Transfur";
         }
-        return "\u00A7fYou are a Transfur";
+
+        if (formId.contains("wolf") || formId.contains("dog")) {
+            return "§fYou are a Dog Transfur";
+        }
+
+        return "§fYou are a Transfur";
     }
 }
