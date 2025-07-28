7aece3a5e6e250d5f5939c9c887180cd74e95b6b
0SeNiA0 <61203056+0SeNiA0@users.noreply.github.com>
2025-07-29 00:15:31 +0200

    AccessoryEntityProvider

--- src/main/java/net/foxyas/changedaddon/datagen/AccessoryEntityProvider.java
+++ src/main/java/net/foxyas/changedaddon/datagen/AccessoryEntityProvider.java
@@ -1,62 +1,130 @@
 package net.foxyas.changedaddon.datagen;
 
-import com.google.common.collect.Multimap;
-import com.google.common.collect.HashMultimap;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import net.ltxprogrammer.changed.Changed;
+import com.mojang.logging.LogUtils;
+import it.unimi.dsi.fastutil.objects.ObjectArraySet;
+import net.foxyas.changedaddon.ChangedAddonMod;
+import net.ltxprogrammer.changed.data.AccessorySlotType;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.HashCache;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
-public class AccessoryEntityProvider { /*
+public class AccessoryEntityProvider implements DataProvider {
 
-//TODO
-} implements DataProvider {
+    private static final Logger LOGGER = LogUtils.getLogger();
+    private static final Gson GSON = new Gson();
     private final DataGenerator generator;
-    private final Map<ResourceLocation, Multimap<EntityType<?>, ResourceLocation>> data = new HashMap<>();
+    protected final String modId;
+    private final Map<String, Appender> appenders = new HashMap<>();
 
     public AccessoryEntityProvider(DataGenerator generator) {
-        this.generator = generator;
+        this(generator, ChangedAddonMod.MODID);
     }
 
-    public void add(ResourceLocation id, EntityType<?> entityType, ResourceLocation slotId) {
-        this.data.computeIfAbsent(id, k -> HashMultimap.create()).put(entityType, slotId);
+    public AccessoryEntityProvider(DataGenerator generator, String modId) {
+        this.generator = generator;
+        this.modId = modId;
     }
 
     @Override
-    public void run(HashCache cache) throws IOException {
-        Path basePath = this.generator.getOutputFolder().resolve("accessories/entities");
+    public void run(@NotNull HashCache cache) {
+        registerEntityAccessories();
 
-        for (var entry : data.entrySet()) {
-            JsonObject root = new JsonObject();
-            JsonArray entities = new JsonArray();
-            JsonArray slots = new JsonArray();
+        Path basePath = this.generator.getOutputFolder();
+        Appender appender;
+        for(Map.Entry<String, Appender> entry : appenders.entrySet()){
+            appender = entry.getValue();
 
-            entry.getValue().asMap().forEach((entityType, slotSet) -> {
-                entities.add(EntityType.getKey(entityType).toString());
-                slotSet.forEach(slotId -> slots.add(slotId.toString()));
-            });
+            if(appender.isInvalid()){
+                LOGGER.error("{} Provider: Appender for file {} is missing entities or slots!", getName(), entry.getKey());
+                continue;
+            }
 
-            root.add("entities", entities);
-            root.add("slots", slots);
-
-            Path path = basePath.resolve(entry.getKey().getPath() + ".json");
-            DataProvider.save(new Gson().from, cache, root, path);
+            Path path = createPath(basePath, entry.getKey());
+            try {
+                DataProvider.save(GSON, cache, appender.toJson(), path);
+            } catch (IOException e) {
+                LOGGER.error("Couldn't save accessory entities to file {}", path, e);
+            }
         }
     }
 
+    public Appender add(@NotNull String fileName){
+        return appenders.computeIfAbsent(fileName, f -> new Appender());
+    }
+
+    protected void registerEntityAccessories(){
+
+    }
+
+    private Path createPath(Path base, String fileName){
+        return base.resolve("data/" + modId + "/changed/accessories/entities/" + fileName + ".json");
+    }
+
     @Override
-    public String getName() {
+    public @NotNull String getName() {
         return "Accessory Entities";
-    }*/
+    }
+
+    @ParametersAreNonnullByDefault
+    public static class Appender {
+
+        private final Set<EntityType<?>> entities = new HashSet<>();
+        private final Set<AccessorySlotType> slots = new ObjectArraySet<>();
+
+        private Appender(){}
+
+        public Appender entity(EntityType<?> entity){
+            entities.add(entity);
+            return this;
+        }
+
+        public Appender entities(EntityType<?> ... entities){
+            Collections.addAll(this.entities, entities);
+            return this;
+        }
+
+        public Appender slot(AccessorySlotType slot){
+            slots.add(slot);
+            return this;
+        }
+
+        public Appender slots(AccessorySlotType... slots){
+            Collections.addAll(this.slots, slots);
+            return this;
+        }
+
+        private boolean isInvalid(){
+            return entities.isEmpty() || slots.isEmpty();
+        }
+
+        private JsonObject toJson(){
+            JsonObject root = new JsonObject();
+            JsonArray entityAr = new JsonArray();
+            JsonArray slotAr = new JsonArray();
+            root.add("entities", entityAr);
+            root.add("slots", slotAr);
+
+            for(EntityType<?> type : entities){
+                entityAr.add(type.getRegistryName().toString());
+            }
+
+            for(AccessorySlotType slot : slots){
+                slotAr.add(slot.getRegistryName().toString());
+            }
+
+            return root;
+        }
+    }
 }
