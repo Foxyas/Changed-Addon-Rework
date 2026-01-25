e5529e42cee061ebbdf999e84d19888444e41988
Cseh Dominik <86685524+xxXMinionXxx@users.noreply.github.com>
2026-01-25 07:19:13 +0100

    Update HULanguageProvider.java

--- src/main/java/net/foxyas/changedaddon/datagen/lang/HULanguageProvider.java
+++ src/main/java/net/foxyas/changedaddon/datagen/lang/HULanguageProvider.java
@@ -270,7 +270,7 @@ public class HULanguageProvider extends LanguageProvider {
         addEntityDialoguesO("exp9.pat.type_2", "§l§3Vedd le rólam a mocskos kezeidet!");
         addEntityDialoguesO("exp9.pat.type_3", "§l§3Simogass meg még egyszer, és addig zúzlak össze, amíg semmi sem marad belőled!");
         addEntityDialoguesO("exp9.reaction.range_attacks", "§l§o§3GYÁVA VAGY! Csak a távolságra tudsz támaszkodni? Milyen SZÁNALMAS!!!");
-        addEntityDialoguesO("exp9.reaction.fire_damage", "Gyufával küzdesz a viharral.... Nem rosszul számoltál – félreértettél.");
+        addEntityDialoguesO("exp9.reaction.fire_damage", "§l§o§3Gyufával küzdesz a viharral.... Nem rosszul számoltál – félreértettél.");
         addEntityDialoguesO("exp10.pat.type_0", "§l§4Igen, VÉGET VETEK A KIBASZOTT ÉLETEDNEK");
         addEntityDialoguesO("exp10.pat.type_1", "§l§4Nem fogod sokáig megbánni – meghalsz érte.");
         addEntityDialoguesO("exp10.pat.type_2", "§l§4Érj meg újra, és gyorsan csinálom... neked.");
