b2e7dbe20ddee21159bc922b397c6669d22bf932
Cseh Dominik <86685524+xxXMinionXxx@users.noreply.github.com>
2026-01-30 10:08:00 +0100

    Update Hungarian Translation

--- src/main/java/net/foxyas/changedaddon/datagen/lang/HULanguageProvider.java
+++ src/main/java/net/foxyas/changedaddon/datagen/lang/HULanguageProvider.java
@@ -441,6 +441,8 @@ public class HULanguageProvider extends LanguageProvider {
         addGui("transfur_sounds_gui.purr", "Dorombol");
         addGui("transfur_sounds_gui.purreow", "Dorombolva nyávog");
         addGui("transfur_sounds_gui.roar", "Ordít");
+        addGui("transfur_sounds_gui.spider_ambient", "Sziszeg");
+        addGui("transfur_sounds_gui.spider_crawl", "Mászik");
         addGui("transfur_sounds_gui.swim", "Úszik");
         addGui("transfur_sounds_gui.whine", "Vinnyog");
         addGui("transfur_sounds_gui.yip", "Csahol");
