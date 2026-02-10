570e4425d1ffc2667a031763a859ba51e5c9c063
Foxyas <CarlosDeck9474@gmail.com>
2026-02-09 21:30:36 -0300

    tweaks and added chest loot datagen

---
+++ src/main/java/net/foxyas/changedaddon/datagen/ChestLoot.java
@@ -0,0 +1,14 @@
+package net.foxyas.changedaddon.datagen;
+
+import net.minecraft.data.loot.LootTableSubProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.storage.loot.LootTable;
+
+import java.util.function.BiConsumer;
+
+public class ChestLoot implements LootTableSubProvider {
+    @Override
+    public void generate(BiConsumer<ResourceLocation, LootTable.Builder> pOutput) {
+
+    }
+}
