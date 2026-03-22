c4a6b089675f6368789c240637c6c3104f1d2ad1
0SeNiA0 <61203056+0SeNiA0@users.noreply.github.com>
2026-03-22 11:00:45 +0100

    fixed SummonEntityProcedure

--- src/main/java/net/foxyas/changedaddon/procedure/SummonEntityProcedure.java
+++ src/main/java/net/foxyas/changedaddon/procedure/SummonEntityProcedure.java
@@ -16,7 +16,7 @@ import net.minecraftforge.event.ForgeEventFactory;
 public class SummonEntityProcedure {
 
     public static void execute(Level world, Player player) {
-        if (!player.level.isClientSide() || !(world instanceof ServerLevel level)) return;
+        if (player.level.isClientSide() || !(world instanceof ServerLevel level)) return;
 
         TransfurVariantInstance<?> instance = ProcessTransfur.getPlayerTransfurVariant(player);
         if (instance == null) return;
