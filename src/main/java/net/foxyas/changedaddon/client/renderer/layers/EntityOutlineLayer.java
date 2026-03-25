8eaf19074a1e1bc5ed50a01c58712b2fc9f5d053
Foxyas <CarlosDeck9474@gmail.com>
2026-03-25 15:22:28 -0300

    Update EntityOutlineLayer.java
    added first person render

--- src/main/java/net/foxyas/changedaddon/client/renderer/layers/EntityOutlineLayer.java
+++ src/main/java/net/foxyas/changedaddon/client/renderer/layers/EntityOutlineLayer.java
@@ -3,15 +3,20 @@ package net.foxyas.changedaddon.client.renderer.layers;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.foxyas.changedaddon.client.renderer.renderTypes.ChangedAddonRenderTypes;
+import net.ltxprogrammer.changed.client.FormRenderHandler;
+import net.ltxprogrammer.changed.client.renderer.layers.FirstPersonLayer;
 import net.ltxprogrammer.changed.client.renderer.model.AdvancedHumanoidModel;
 import net.ltxprogrammer.changed.entity.ChangedEntity;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.world.entity.HumanoidArm;
 import org.jetbrains.annotations.NotNull;
 
-public class EntityOutlineLayer<M extends AdvancedHumanoidModel<T>, T extends ChangedEntity> extends RenderLayer<T, M> {
+public class EntityOutlineLayer<M extends AdvancedHumanoidModel<T>, T extends ChangedEntity> extends RenderLayer<T, M> implements FirstPersonLayer<T> {
 
     public EntityOutlineLayer(RenderLayerParent<T, M> parent) {
         super(parent);
@@ -34,5 +39,26 @@ public class EntityOutlineLayer<M extends AdvancedHumanoidModel<T>, T extends Ch
         );
 
         poseStack.popPose();
+    }
+
+    @Override
+    public void renderFirstPersonOnArms(PoseStack stack, MultiBufferSource bufferSource, int packedLight, T entity, HumanoidArm arm, PartPose armPose, float partialTick) {
+        FirstPersonLayer.super.renderFirstPersonOnArms(stack, bufferSource, packedLight, entity, arm, armPose, partialTick);
+        stack.pushPose();
+        stack.scale(1.01F, 1.01F, 1.01F);
+        VertexConsumer outlineBuffer = bufferSource.getBuffer(ChangedAddonRenderTypes.outlineWithDepth(this.getTextureLocation(entity)));
+        AdvancedHumanoidModel<T> armedModel = this.getParentModel();
+        ModelPart armPart = armedModel.getArm(arm);
+        armPart.loadPose(armPose);
+        FormRenderHandler.renderModelPartWithTexture(armedModel.getArm(arm),
+                stack,
+                outlineBuffer,
+                packedLight,
+                1,
+                1,
+                1,
+                1.0F);
+
+        stack.popPose();
     }
 }
