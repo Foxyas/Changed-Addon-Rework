a8776f131dd5a34d62170331723ff8512a6081e2
0SeNiA0 <61203056+0SeNiA0@users.noreply.github.com>
2026-03-25 17:08:21 +0100

    dont send unfuse message if player wasnt tfed

--- src/main/java/net/foxyas/changedaddon/item/DiffusionSyringeItem.java
+++ src/main/java/net/foxyas/changedaddon/item/DiffusionSyringeItem.java
@@ -1,8 +1,11 @@
 package net.foxyas.changedaddon.item;
 
 import net.foxyas.changedaddon.init.ChangedAddonTabs;
+import net.foxyas.changedaddon.network.ChangedAddonVariables;
 import net.foxyas.changedaddon.procedure.SummonEntityProcedure;
+import net.foxyas.changedaddon.util.ComponentUtil;
 import net.foxyas.changedaddon.util.PlayerUtil;
+import net.ltxprogrammer.changed.process.ProcessTransfur;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -23,10 +26,16 @@ public class DiffusionSyringeItem extends AbstractSyringeItem {
     @Override
     public void applyEffectsAfterUse(@NotNull ItemStack pStack, Level level, LivingEntity entity) {
         super.applyEffectsAfterUse(pStack, level, entity);
-        if (entity instanceof Player player) {
+
+        if (!(entity instanceof Player player)) return;
+
+        if (ProcessTransfur.isPlayerTransfurred(player)) {
             SummonEntityProcedure.execute(level, player);
             PlayerUtil.UnTransfurPlayerAndPlaySound(player, !player.isCreative() && !player.isSpectator());
             player.displayClientMessage(new TranslatableComponent("changed_addon.untransfur.diffusion"), true);
+            return;
         }
+
+        if (ChangedAddonVariables.ofOrDefault(player).showWarns) player.displayClientMessage(ComponentUtil.translatable("changed_addon.untransfur.no_effect"), true);
     }
 }
