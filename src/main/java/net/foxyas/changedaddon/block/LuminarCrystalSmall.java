1daa637f2731dbc1ebb77fd8552e1a89ee85bb1f
Foxyas <CarlosDeck9474@gmail.com>
2026-04-26 18:28:54 -0300

    Update LuminarCrystalSmall.java

--- src/main/java/net/foxyas/changedaddon/block/LuminarCrystalSmall.java
+++ src/main/java/net/foxyas/changedaddon/block/LuminarCrystalSmall.java
@@ -360,6 +360,10 @@ public class LuminarCrystalSmall extends TransfurCrystalBlock implements SimpleW
 
     @Override
     public void onBrush(Level level, BlockState state, BlockPos pos, Player player, Direction side, ItemStack brushStack) {
-
+        if (!level.isClientSide()) {
+            if (player.getRandom().nextFloat() >= 0.05f) {
+                Block.popResource(level, pos, ChangedAddonItems.LUMINAR_CRYSTAL_SHARD.get().getDefaultInstance());
+            }
+        }
     }
 }
