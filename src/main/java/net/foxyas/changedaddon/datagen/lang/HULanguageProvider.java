d153081a8ebfeb1e30bc162a0fcdc528e57eebac
Cseh Dominik <86685524+xxXMinionXxx@users.noreply.github.com>
2026-06-06 07:12:20 +0200

    Update Hungarian Translation

--- src/main/java/net/foxyas/changedaddon/datagen/lang/HULanguageProvider.java
+++ src/main/java/net/foxyas/changedaddon/datagen/lang/HULanguageProvider.java
@@ -176,6 +176,9 @@ public class HULanguageProvider extends LanguageProvider {
         addBlock(DARK_LATEX_WOLF_PLUSHY, "Plüss sötét latex farkas");
         addBlock(YELLOW_WOLF_CRYSTAL_BLOCK, "Sárga farkas kristályblokk");
         addBlock(YELLOW_WOLF_CRYSTAL_SMALL, "Kicsi sárga farkas kristály");
+        addBlock(LUMINARA_LOG, "Luminararönk");
+        addBlock(STRIPPED_LUMINARA_LOG, "Kérgezett luminararönk");
+        addBlock(LUMINARA_LEAVES, "Luminaralevelek");
 
         addAbility("advanced_hearing", "Fejlett hallás");
         addAbility("carry", "Hordozás");
