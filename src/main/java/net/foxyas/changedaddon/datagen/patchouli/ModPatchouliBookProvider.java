121a2ea80e7a0da55b1f96a350a4d75f1baf1270
Foxyas <CarlosDeck9474@gmail.com>
2026-06-30 13:14:48 -0300

    tweaks for the PatchouliBook of the mod

--- src/main/java/net/foxyas/changedaddon/datagen/patchouli/ModPatchouliBookProvider.java
+++ src/main/java/net/foxyas/changedaddon/datagen/patchouli/ModPatchouliBookProvider.java
@@ -1,9 +1,11 @@
 package net.foxyas.changedaddon.datagen.patchouli;
 
 import net.foxyas.changedaddon.ChangedAddonMod;
+import net.foxyas.changedaddon.init.ChangedAddonCreativeTabs;
 import net.foxyas.changedaddon.init.ChangedAddonItems;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import xyz.brassgoggledcoders.patchouliprovider.BookBuilder;
 import xyz.brassgoggledcoders.patchouliprovider.CategoryBuilder;
 import xyz.brassgoggledcoders.patchouliprovider.PatchouliBookProvider;
@@ -20,25 +22,34 @@ public class ModPatchouliBookProvider extends PatchouliBookProvider {
     protected void addBooks(Consumer<BookBuilder> consumer) {
         BookBuilder book = createBookBuilder(
                 "guidebook",
-                "Changed Addon Guide",
+                "§3Changed Addon Guide",
                 "Welcome to the Changed Addon guidebook!"
         );
+        book.setCustomBookItem(ChangedAddonItems.CHANGED_BOOK.get().getDefaultInstance());
         book.setUseResourcePack(true);
         book.setI18n(true);
+        book.setDontGenerateBook(false);
+        book.setShowProgress(false);
+        book.setShowToasts(true);
+        book.setCreativeTab(ChangedAddonCreativeTabs.CHANGED_ADDON_MAIN_TAB.getId().toString());
+        book.setModel(ChangedAddonItems.CHANGED_BOOK.getId());
+        book.setShowToasts(true);
 
         ItemStack lunarRose = ChangedAddonItems.LUNAR_ROSE.get().getDefaultInstance();
-        CategoryBuilder mainCategory = book.addCategory(
-                "main",
-                "Main",
-                "General information about the mod.",
-                lunarRose
+        ItemStack paper = Items.PAPER.getDefaultInstance();
+        CategoryBuilder riddles = book.addCategory(
+                "riddles",
+                "Changed Addon Riddles",
+                "patchouli_descriptions.changed_addon.riddles",
+                paper
         );
 
-        mainCategory.addEntry(
-                "lunar_rose_poem",
-                "Lunar Rose Poem",
-                lunarRose
-        ).addSimpleTextPage("jei_descriptions.changed_addon.lunar_rose");
+        riddles.addEntry(
+                        "lunar_rose_poem",
+                        "Lunar Rose Poem",
+                        lunarRose
+                )
+                .addSimpleTextPage("patchouli_descriptions.changed_addon.lunar_rose");
 
         consumer.accept(book);
     }
