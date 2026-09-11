e7b7c0403f4b00beda66e27dc139470744e0eea4
Cseh Dominik <86685524+xxXMinionXxx@users.noreply.github.com>
2026-09-11 09:16:24 +0200

    Update Hungarian Translation

--- src/main/java/net/foxyas/changedaddon/datagen/lang/HULanguageProvider.java
+++ src/main/java/net/foxyas/changedaddon/datagen/lang/HULanguageProvider.java
@@ -497,7 +497,8 @@ public class HULanguageProvider extends LanguageProvider {
         addGui("unifuser_gui.tooltip.place_second_ingredient", "Tedd ide a második hozzávalót");
         addGui("abilities_radial_screen.mouse.right_click", "Nyomd meg a %s gombot a második képesség kiválasztásához");
 
-        add("ability.changed.grab_entity.desc.toggle_grab_safe_mode", "Nyomja meg a %s vagy %s gombot a barátságos megragadás bekapcsolásához");
+        add("ability.changed.grab_entity.desc.toggle_grab_safe_mode", "Nyomja meg a %s vagy a %s gombot a barátságos megragadás kapcsolásához");
+        add("ability.changed.grab_entity.desc.toggle_grab_transfur_damage_mode", "Nyomd meg a %s vagy a %s gombot a megragadáskori transzfur sebzés kapcsolásához");
 
         add("deathScreen.select_tf", "Újraéledés transzfurként?");
         add("deathScreen.select_tf.hardcore", "Folytatás transzfurként?");
@@ -691,8 +692,9 @@ public class HULanguageProvider extends LanguageProvider {
         add("key.changed_addon.pat_message", "Simogatod %s entitást");
         add("key.changed_addon.pat_received", "%s téged simogat");
         addKey(TURN_OFF_TRANSFUR, "Transzfur kikapcsolása");
-        add("key.changed_addon.turn_off_transfur.grab_safe_mode", "Barátságos megragadás beállítva: %s");
-        add("key.changed_addon.turn_off_transfur.safe_mode", "Transzfur támadás beállítva: %s");
+        add("key.changed_addon.turn_off_transfur.grab_safe_mode", "Barátságos megragadás beállítása erre: %s");
+        add("key.changed_addon.turn_off_transfur.grab_transfur_damage_mode", "Megragadáskori transzfur sebzés beállítása erre: %s");
+        add("key.changed_addon.turn_off_transfur.safe_mode", "Transzfur támadás beállítása erre: %s");
         addKey(USE_SECOND_ABILITY, "Változat második képességének használata");
         addKey(CUDDLE_KEY, "Ölelkezési mód váltása");
         addKey(CUDDLE_KEY, "set", "Ölelkezési mód beállítása: %s");
