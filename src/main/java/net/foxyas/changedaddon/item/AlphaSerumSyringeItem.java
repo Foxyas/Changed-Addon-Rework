7dbc5d5df8c6c560b049226d3d544f5304fe8eeb
0SeNiA0 <61203056+0SeNiA0@users.noreply.github.com>
2026-09-23 22:30:52 +0200

    attacking with alpha serum syringe will inject it in the target granted target is valid

--- src/main/java/net/foxyas/changedaddon/item/AlphaSerumSyringeItem.java
+++ src/main/java/net/foxyas/changedaddon/item/AlphaSerumSyringeItem.java
@@ -3,10 +3,12 @@ package net.foxyas.changedaddon.item;
 import net.foxyas.changedaddon.entity.api.IAlphaAbleEntity;
 import net.foxyas.changedaddon.network.ChangedAddonVariables;
 import net.foxyas.changedaddon.util.MathFormulasUtil;
+import net.ltxprogrammer.changed.ability.IAbstractChangedEntity;
 import net.ltxprogrammer.changed.entity.ChangedEntity;
 import net.ltxprogrammer.changed.entity.variant.TransfurVariantInstance;
 import net.ltxprogrammer.changed.process.ProcessTransfur;
 import net.minecraft.network.chat.Component;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -22,6 +24,17 @@ public class AlphaSerumSyringeItem extends AbstractSyringeItem {
                 .stacksTo(64)
                 .rarity(Rarity.RARE)
         );
+    }
+
+    @Override
+    public boolean hurtEnemy(@NotNull ItemStack stack, @NotNull LivingEntity target, @NotNull LivingEntity attacker) {
+        IAbstractChangedEntity entity = IAbstractChangedEntity.forEitherSafe(target).orElse(null);
+        if (entity == null || !(entity.getChangedEntity() instanceof IAlphaAbleEntity alphaAble) || alphaAble.isAlpha()) return false;
+
+        stack.shrink(1);
+        alphaAble.setAlpha(true);
+        alphaAble.setAlphaScale(alphaScale(target.getRandom()));
+        return true;
     }
 
     @Override
@@ -53,9 +66,11 @@ public class AlphaSerumSyringeItem extends AbstractSyringeItem {
         }
 
         iAlphaAbleEntity.setAlpha(true);
-        float rand = player.getRandom().nextFloat();
-        float alphaScale = MathFormulasUtil.lerpEase(rand, 0.5f, 2.5f, MathFormulasUtil.EasingType.QUAD_IN);
-        iAlphaAbleEntity.setAlphaScale(alphaScale);
+        iAlphaAbleEntity.setAlphaScale(alphaScale(player.getRandom()));
+    }
+
+    protected float alphaScale(RandomSource random) {
+        return MathFormulasUtil.lerpEase(random.nextFloat(), 0.5f, 2.5f, MathFormulasUtil.EasingType.QUAD_IN);
     }
 
     private void failMessage(Player player) {
