cfa1210ed5b3091fad23042a1e0ec3a8ded0f100
Foxyas <119010731+Foxyas@users.noreply.github.com>
2026-09-24 14:48:16 -0300

    synced pos

--- src/main/java/net/foxyas/changedaddon/mixins/entity/variant/LostControlTransfurVariantInstanceMixin.java
+++ src/main/java/net/foxyas/changedaddon/mixins/entity/variant/LostControlTransfurVariantInstanceMixin.java
@@ -20,6 +20,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -141,8 +142,8 @@ public abstract class LostControlTransfurVariantInstanceMixin<T extends ChangedE
                 player.setLastHurtByPlayer(((LivingEntityAccessor)entityInControl).ChangedAddon$getLastHurtByPlayer());
                 player.setLastHurtMob(entityInControl.getLastHurtMob());
                 player.setForcedPose(entityInControl.getPose());
-//                Vec3 position = entityInControl.position();
-//                player.teleportTo(position.x, position.y, position.z);
+                Vec3 position = entityInControl.position();
+                player.absMoveTo(position.x, position.y, position.z);
             } else if (this.getTransfurProgression(0) >= 1f) {
                 if (!host.level().isClientSide()) {
                     ChangedAddon$generateEntityInControl();
